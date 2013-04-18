95dc20de2f5eb04c4f4c03404b8480758f25c856
Elliott Seyler <emhs08@gmail.com>
2013-04-18 03:54:56 -0700

    Added main loop and map window rendering

--- runes.py
+++ runes.py
@@ -6,6 +6,7 @@ from runes import map
 from runes import creature
 
 def main(stdscr):
+    curses.cbreak()
     curses.curs_set(0)
     # Initialize map_window to display the map
     map_height = 21
@@ -15,7 +16,44 @@ def main(stdscr):
     map_window = curses.newwin(map_height, map_width, map_start_y, map_start_x)
     map_window.border()
     
+    # Initialize map and player
+    active_map = map.Map(architecture.Cell())
+    player = creature.Player()
+    player._bamf(active_map[8][8])
     
+    # Initial render
+    map_render = active_map.render()
+    for row in range(21):
+        for col in range(80):
+            map_window.addstr(row, col, *map_render[row][col])
+    
+    # Main loop
+    char = stdscr.getkey()
+    while char != 'Q':
+        if char == 'h':
+            player.go_west()
+        elif char == 'j':
+            player.go_south()
+        elif char == 'k':
+            player.go_north()
+        elif char == 'l':
+            player.go_east()
+        elif char == 'u':
+            player.go_northeast()
+        elif char == 'y':
+            player.go_northwest()
+        elif char == 'n':
+            player.go_southeast()
+        elif char == 'b':
+            player.go_southwest()
+        # Re-render map
+        map_render = active_map.render()
+        for row in range(21):
+            for col in range(80):
+                map_window.addstr(row, col, *map_render[row][col])
+        char = stdscr.getkey()
+    
+    curses.nocbreak()
 
 if __name__ == '__main__':
     curses.wrapper.wrapper(main)
