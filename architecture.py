c0f8051d6057097010a4cbaddc0f0b3da37c986e
Elliott Seyler <emhs08@gmail.com>
2013-04-18 01:51:37 -0700

    Added Cell docstring and adjacent cells attribute

--- architecture.py
+++ architecture.py
@@ -1,6 +1,34 @@
+class Error(Exception):
+    """Base class for creature exceptions"""
+    pass
+
+class MovementBlocked(Error):
+    """Exception raised for movement to a blocked destination cell.
+    
+    Attributes:
+        dest -- blocked destination cell
+    """
+    def __init__(self, dest):
+        self.dest = dest
+
 import curses
 
 class Cell():
+    """Base map cell.
+    
+    Attributes:
+        creature        -- creature (if any) in the cell
+        contents        -- list of items within the cell
+        inscriptions    -- runic inscriptions on this cell
+        adjacent        -- list of adjacent cells (clockwise starting at north)
+    
+    Flags:
+        open            -- cell passable
+        lit             -- cell lit by a light source
+        glow            -- cell permanently lit by its own light source
+        seen            -- ceel is in player FOV
+    """
+    
     def __init__(self, contents=[], inscriptions=[], creature=None, open=True, 
             lit=False, seen=False, glow=False):
         self.open = open # Cell passabiliy
@@ -10,6 +38,7 @@ class Cell():
         self.creature = creature # Creature (if any) occupying the cell
         self.contents = contents # Objects contained within the cell
         self.inscriptions = inscriptions # Runic inscriptions on this cell
+        self.adjacent = []
     
 def wall(contents=[], inscriptions=[], creature=[], open=False, lit=False, 
         seen=False, glow=False):
