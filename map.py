7319fde3e35d85a5a6b9624e6305a7da8b172721
Elliott Seyler <emhs@wolfman.devio.us>
2013-04-20 06:19:11 -0500

    Added draw_line and draw_shape methods for Map, building on draw_point

--- map.py
+++ map.py
@@ -50,3 +50,46 @@ class Map():
             self.map[point[0]][point[1]].open = False
         elif type == 'floor':
             self.map[point[0]][point[1]].open = True
+
+    def draw_line(self, start, end, type='wall'):
+        if start[0] == end[0]:
+            for x in range(start[1], end[1]):
+                self.draw_point((start[0], x), type=type)
+        elif start[1] == end[1]:
+            for y in range(start[0], end[0]):
+                self.draw_point((y, start[1]), type=type)
+        else:
+            if start[0] > end[0]:
+                y0 = end[0]
+                y1 = start[0]
+                x0 = end[1]
+                x1 = start[1]
+            else:
+                y0 = start[0]
+                y1 = end[0]
+                x0 = start[1]
+                y0 = end[1]
+            dy = y1 - y0
+            dx = x1 - x0
+            slope = round(dy/dx)
+            if dx < 0:
+                sign = -1
+            else:
+                sign = 1
+            if dy>dx:
+                xx = 0
+                for x in range(x0, x1+1, sign):
+                    for y in range(y0+xx*slope, y1+(xx+1)*slope)
+                        self.draw_point((y,x), type=type)
+                    xx += 1
+            elif dx>dy:
+                yy = 0
+                for y in range(y0, y1+1):
+                    for x in range(x0+yy/slope, x1+(yy+1)/slope):
+                        self.draw_point((y,x), type=type)
+    
+    def draw_shape(self, points, closed=False, type='wall'):
+        for ii in range(len(points)-1):
+            self.draw_line(points[ii], points[ii+1], type=type)
+        if closed:
+            self.draw_line(points[-1], points[0], type=type)
